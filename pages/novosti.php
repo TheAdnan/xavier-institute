cbce8f129e7ddfe19a6bf5ccbe59133cd93266d0
TheAdnan <adnankicin92@gmail.com>
2016-05-19 19:55:53 +0200

    sve novosti u csv

--- pages/novosti.php
+++ pages/novosti.php
@@ -88,93 +88,34 @@
 			<option value="mjesec" id="ovog-mjeseca">Novosti ovog mjeseca</option>
 			<option value="sve" id="sve" selected="selected">Sve novosti</option>
 		</select> 
-		
-		<input type='submit' name='sortiraj' class='sortiraj' onclick='sortuj();' value='Sortiraj po abecedi' />
-		<input type='submit' name='sortirajDate' class='sortiraj' onclick='sortujPoDatumu();' value='Sortiraj po datumu' />
+		<form action='novosti.php' method="POST">
+		<input type='submit' name='sortiraj' class='sortiraj' value='Sortiraj po abecedi' />
+		<input type='submit' name='sortirajDate' class='sortiraj' value='Sortiraj po datumu' /></form>
 				<br>
 				<br>
 				<br>
-						<div class="nowost">
-							<h3>Film "Deadpool" pokorio američki box office</h3>
-							<p class="objavljeno">Objavljeno<time class="vrijemeObjave" datetime="2016-05-19T09:09:00"></time>.</p>
-							<img src="../images/vijest1.jpg" alt="Deadpool">
-							<p>Film "Deadpool" je na svom debiju pokorio američki box office zaradivši 135 miliona dolara. Zahvaljujući odličnom debiju u kinima, film "Deadpool" je postao najuspješniji od svih koji nose oznaku R, što znači da je zabranjen za osobe mlađe od 17 godina...</p>
-						</div>
-						<div class="nowost">
-							<h3>Prvi X-Men Apocalypse trailer</h3>
-							<p class="objavljeno">Objavljeno<time class="vrijemeObjave" datetime="2016-05-19T00:45:00"></time>.</p>
-							<img src="../images/vijest2.jpg" alt="Apocalypse">
-							<p>Ako ste gledajući X-Men: Days of Future Past odsjedili iza završnice, imali ste priliku vidjeti kraći teaser za idući nastavak koji nosi naziv X-Men: Apocalypse. Špekulacije su od tada potvrđene pa znamo da će se X-Men ekipa u novom filmu boriti protiv Apocalypsea i njegova četiri jahača...</p>
-						</div>
-						<div class="nowost">
-							<h3>Apocalypse Sneak Peek</h3>
-							<p class="objavljeno">Objavljeno<time class="vrijemeObjave" datetime="2016-05-18T20:00:00"></time>.</p>
-							<img src="../images/apocalypse.jpg" alt="apocalypse-1">
-							<p>Film "Deadpool" je na svom debiju pokorio američki box office zaradivši 135 miliona dolara. Zahvaljujući odličnom debiju u kinima, film "Deadpool" je postao najuspješniji od svih koji nose oznaku R, što znači da je zabranjen za osobe mlađe od 17 godina...</p>
-						</div>
-						<div class="nowost">
-							<h3>Apocalypse poster #1</h3>
-							<p class="objavljeno">Objavljeno<time class="vrijemeObjave" datetime="2016-03-20T13:45:00"></time>.</p>
-							<img src="../images/apocalypse-1.jpg" alt="Apocalypse">
-							<p>Ako ste gledajući X-Men: Days of Future Past odsjedili iza završnice, imali ste priliku vidjeti kraći teaser za idući nastavak koji nosi naziv X-Men: Apocalypse. Špekulacije su od tada potvrđene pa znamo da će se X-Men ekipa u novom filmu boriti protiv Apocalypsea i njegova četiri jahača...</p>
-						</div>
-						<div class="nowost">
-							<h3>Behind the scenes slike</h3>
-							<p class="objavljeno">Objavljeno<time class="vrijemeObjave" datetime="2016-03-27T20:10:00"></time>.</p>
-							<img src="../images/behind-the-scenes.jpg" alt="Deadpool">
-							<p>Film "Deadpool" je na svom debiju pokorio američki box office zaradivši 135 miliona dolara. Zahvaljujući odličnom debiju u kinima, film "Deadpool" je postao najuspješniji od svih koji nose oznaku R, što znači da je zabranjen za osobe mlađe od 17 godina...</p>
-						</div>
-						<div class="nowost">
-							<h3>Charles and Eric poster</h3>
-							<p class="objavljeno">Objavljeno<time class="vrijemeObjave" datetime="2016-03-30T23:45:00"></time>.</p>
-							<img src="../images/eric-xavier.jpg" alt="Apocalypse">
-							<p>Ako ste gledajući X-Men: Days of Future Past odsjedili iza završnice, imali ste priliku vidjeti kraći teaser za idući nastavak koji nosi naziv X-Men: Apocalypse. Špekulacije su od tada potvrđene pa znamo da će se X-Men ekipa u novom filmu boriti protiv Apocalypsea i njegova četiri jahača...</p>
-						</div>
-					<div class="nowost">
-							<h3>Prijemni ispit na Xavier's School for Gifted Youngsters</h3>
-							<p class="objavljeno">Objavljeno<time class="vrijemeObjave" datetime="2016-03-30T00:11:00"></time>.</p>
-							<img src="../images/vijest3.jpg" alt="Deadpool and Negasonic">
-							<p>Na osnovu člana 125. Zakona o visokom obrazovanju ("Službene novine Kantona Sarajevo", br. 42/13 - prečišćeni tekst i 13/15), člana 95. Statuta Xavier Instituta u New Yorku i tačke III Odluke Vlade o davanju...</p>
-						</div>
-					<div class="nowost">
-							<h3>Biografija: Profesor Charles Francis Xavier</h3>
-							<p class="objavljeno">Objavljeno<time class="vrijemeObjave" datetime="2016-03-16T13:04:00"></time>.</p>
-							<img src="../images/vijest4.jpg" alt="Charles Xavier">
-							<p>Charles Francis Xavier rođen je u New Yorku, oca Brian Xavier i majke, cijenjene naučnice u oblasti nuklearne fizike, Sharon Xavier. Nakon što je izgubio oca u saobraćajnoj nesreći, njegov prijatelj Kurt Marko stupa u brak s Sharon...</p>
-						</div>
-					<div class="nowost">
-							<h3>Trumpocalypse</h3>
-							<p class="objavljeno">Objavljeno<time class="vrijemeObjave" datetime="2012-03-30T00:11:00"></time>.</p>
-							<img src="../images/trumpocalypse.jpg" alt="Deadpool and Negasonic">
-							<p>Na osnovu člana 125. Zakona o visokom obrazovanju ("Službene novine Kantona Sarajevo", br. 42/13 - prečišćeni tekst i 13/15), člana 95. Statuta Xavier Instituta u New Yorku i tačke III Odluke Vlade o davanju...</p>
-						</div>
-					<div class="nowost">
-							<h3>Biografija: Wolverine</h3>
-							<p class="objavljeno">Objavljeno<time class="vrijemeObjave" datetime="2016-03-29T13:14:00"></time>.</p>
-							<img src="../images/wolverine.jpg" alt="Charles Xavier">
-							<p>Charles Francis Xavier rođen je u New Yorku, oca Brian Xavier i majke, cijenjene naučnice u oblasti nuklearne fizike, Sharon Xavier. Nakon što je izgubio oca u saobraćajnoj nesreći, njegov prijatelj Kurt Marko stupa u brak s Sharon...</p>
-						</div>
-					<div class="nowost">
-							<h3>X-Men Posteri - Drugi dio</h3>
-							<p class="objavljeno">Objavljeno<time class="vrijemeObjave" datetime="2016-03-30T00:11:00"></time>.</p>
-							<img src="../images/x-men-poster.jpg" alt="Deadpool and Negasonic">
-							<p>Na osnovu člana 125. Zakona o visokom obrazovanju ("Službene novine Kantona Sarajevo", br. 42/13 - prečišćeni tekst i 13/15), člana 95. Statuta Xavier Instituta u New Yorku i tačke III Odluke Vlade o davanju...</p>
-						</div>
-					<div class="nowost">
-							<h3>X-Men Posteri - Prvi dio</h3>
-							<p class="objavljeno">Objavljeno<time class="vrijemeObjave" datetime="2016-04-01T13:04:00"></time>.</p>
-							<img src="../images/x-men-poster-2.jpg" alt="Charles Xavier">
-							<p>Charles Francis Xavier rođen je u New Yorku, oca Brian Xavier i majke, cijenjene naučnice u oblasti nuklearne fizike, Sharon Xavier. Nakon što je izgubio oca u saobraćajnoj nesreći, njegov prijatelj Kurt Marko stupa u brak s Sharon...</p>
-						</div>
+						
+						
+						
 						<?php
 						$file_handle = fopen("../files/novosti.csv", "r");
-
-						while (!feof($file_handle) ) {
-
-						$cv = fgetcsv($file_handle, 1024);
-
+						if(isset($_POST['sortiraj'])){
 						
-						print "<div class='nowost'><h3>".$cv[0]."</h3><p class='objavljeno'>Objavljeno<time class='vrijemeObjave' datetime='".$cv[3]."T".$cv[4]."'></time>.</p><img src='".$cv[1]."' alt='".$cv[1]."'><p>".$cv[2]."</p></div>";
+						}
+						elseif(isset($_POST['sortirajDate'])){
+							
+						}
+						else{
+							
+						
+							while (!feof($file_handle) ) {
+
+							$cv = fgetcsv($file_handle, 2024);
+							$cv[0]=str_replace(";.?",",",$cv[0]);
+							$cv[2]=str_replace(";.?",",",$cv[2]);
+							
+							print "<div class='nowost'><h3>".$cv[0]."</h3><p class='objavljeno'>Objavljeno<time class='vrijemeObjave' datetime='".$cv[3]."T".$cv[4]."'></time>.</p><img src='".$cv[1]."' alt='".$cv[1]."'><p>".$cv[2]."</p></div>";
+						}
 						}
 						?>
 		</div>
