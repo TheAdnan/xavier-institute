2a48a60c573f326242f5d5dcd2195d45e1e3417b
TheAdnan <adnankicin92@gmail.com>
2016-05-21 02:47:58 +0200

    21052016

---
+++ javascript/validacija.js
@@ -0,0 +1,77 @@
+function validirajUnose(){
+	var naslovE = document.getElementById("naslovvijesti");
+	var tekstE = document.getElementById("tekstvijesti");
+	var slikaE = document.getElementById("slikavijesti");
+	var naslov = document.getElementById("naslovvijesti").value;
+	var tekst = document.getElementById("tekstvijesti").value;
+	var slika = document.getElementById("slikavijesti").value;
+	var validno = false;
+	var reg = /(.+)/igm;
+	if(!(reg.test(naslov))){
+		naslovE.style.backgroundColor = "tomato"; validno=false;
+	}
+	else{
+		naslovE.style.removeProperty('background-color');
+		validno=true;
+	}
+	if(!((/(.+)/).test(tekst))){
+		tekstE.style.backgroundColor = "tomato";
+		validno=false;
+	}
+	else{
+		tekstE.style.removeProperty('background-color');
+		validno=true;
+	}
+	if(!((/(.+)/).test(slika))){
+		slikaE.style.backgroundColor = "tomato";
+		validno=false;
+	}
+	else{
+		slikaE.style.removeProperty('background-color');
+		validno=true;
+	}
+	return validno;
+		
+}
+
+
+function ValidirajKod(){
+	var kod = document.getElementById("kod").value;
+	var ajax = new XMLHttpRequest();
+	ajax.onreadystatechange = function() {
+		var x= document.getElementById("tel");
+		if (ajax.readyState == 4 && ajax.status == 200){
+			var br=JSON.parse(ajax.response);
+			if(br[0]!=null){
+				x.value="+"+br[0]["callingCodes"];
+			}else{
+				x.value="";
+			}
+		}
+		if (ajax.readyState == 4 && ajax.status == 404)
+			document.getElementById("kod").innerHTML = "Ne postoji kod";
+	}
+	ajax.open("GET", "https://restcountries.eu/rest/v1/alpha?codes="+kod, true);
+	ajax.send();	
+}
+
+function ValidirajBrojTelefona(){
+	var tel = document.getElementById("tel").value;
+	tel = tel.substr(1);
+	var ajax = new XMLHttpRequest();
+	ajax.onreadystatechange = function() {
+		if (ajax.readyState == 4 && ajax.status == 200){
+			var br=JSON.parse(ajax.response);
+			if(br[0]!=null){
+				document.getElementById("kod").value="";
+				document.getElementById("kod").value= br[0]["alpha2Code"].toUpperCase();
+			}else{
+				document.getElementById("kod").value="";
+			}
+		}
+		if (ajax.readyState == 4 && ajax.status == 404)
+			document.getElementById("tel").innerHTML = "Nije ispravan pozivni";
+	}
+	ajax.open("GET", "https://restcountries.eu/rest/v1/callingcode/"+tel, true);
+	ajax.send();	
+}
--- pages/admin.php
+++ pages/admin.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" href="../css/stil.css">
 	<link rel="stylesheet" href="../css/logo.css">
 	<script src="../javascript/kod.js"></script>
+	<script src="../javascript/validacija.js"></script>
 </head>
 
 <!--[if lt IE 9]>
@@ -51,18 +52,20 @@
 					<input type='submit' name='posalji' value='Log in' />
 					
 					</fieldset>
-			</form></div>";
+			</form></div>
+			<script>Alert('Logovan si bruda')";
 				header("Location: ../index.php");
 				}
 			if(isset($_SESSION['login'])){
 				print "<div><form id='logout-forma' action='admin.php' method='POST'><input type='submit' name='logout' value='Log out' /></form></div>";
 			
 			}
-		
+				
 				 if(isset($_POST['vijes'])){
 					 $naslov=htmlspecialchars($_REQUEST['naslov']);
 					 $tekst=htmlspecialchars($_POST['tekst']);
 					 $slika=htmlspecialchars($_REQUEST['slika']);
+					
 					 $datum1 = date("Y-m-d");
 					 $datum2 = date("H:i:s");					 
 					 if(!empty($_POST['naslov']) && !empty($_POST['tekst']) && !empty($_POST['slika'])){
@@ -76,7 +79,7 @@
 						 
 					 }
 					 else{
-						 $greska="Niste popunili sva polja!";
+						 print "<script>Alert('Niste popunili sva polja!')</script>";
 					}
 					
 						
@@ -141,21 +144,23 @@
 		
 		<div class="mainbody">
 			<br>
-			<form id="dodaj-novost" action='admin.php' method='POST'>
+			<form id="dodaj-novost" action='admin.php' method='POST' onsubmit="return(validirajUnose());">
 				<label>Naslov vijesti</label>
 				<br>
-				<input type='text' name='naslov' />
+				<input type='text' id='naslovvijesti' oninput='validirajUnose();' name='naslov' />
 				<br>
 				
 				<label>Tekst vijesti</label>
 				<br>
-				<textarea name='tekst' id='tekstvijesti' placeholder="Unesite tekst vijesti"></textarea>
+				<textarea name='tekst' id='tekstvijesti' oninput='validirajUnose();' placeholder="Unesite tekst vijesti"></textarea>
 				<br>
 				<label>URL slike</label>
 				<br>
-				<input type='text' name='slika' />
+				<input type='text' name='slika' id='slikavijesti' oninput='validirajUnose();' />
 				<br>
-				<input type='submit' id='dodajVijestBtn' name='vijes' value="Dodaj vijest"/>
+				<label><small>Kod države</small></label><input type='text' name='kod' id='kod' oninput='ValidirajKod();'/><label><small>Broj telefona</small></label><input type='text' name='tel' id='tel' oninput='ValidirajBrojTelefona();'/>
+				<br>
+				<input type='submit' id='dodajVijestBtn' name='vijes'  value="Dodaj vijest"/>
 				</form>
 				
 				
